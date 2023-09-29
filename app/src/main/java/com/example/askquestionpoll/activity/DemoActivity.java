83efa97bde8bae751db50528579499c8f8cb6688
Admin <parmarabhi3636@gmail.com>
2023-09-29 14:54:31 +0530

    askquestionpoll

--- app/src/main/java/com/example/askquestionpoll/activity/DemoActivity.java
+++ app/src/main/java/com/example/askquestionpoll/activity/DemoActivity.java
@@ -6,17 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 import com.example.askquestionpoll.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DemoActivity extends AppCompatActivity {
+
+    Button button;
     
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
-        
     }
 }
