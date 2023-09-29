4c03ac02eb48658de0caf53b7e78a7b41a9c9eb8
Admin <parmarabhi3636@gmail.com>
2023-09-29 14:40:03 +0530

    askquestionpoll

---
+++ app/src/main/java/com/example/askquestionpoll/activity/DemoActivity.java
@@ -0,0 +1,16 @@
+package com.example.askquestionpoll.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.askquestionpoll.R;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
