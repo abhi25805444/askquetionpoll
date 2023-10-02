dcf3a77a4fd99a8b55455be3e87a807a385618d3
Admin <parmarabhi3636@gmail.com>
2023-10-02 09:54:30 +0530

    askquestionpoll

--- app/src/main/java/com/example/askquestionpoll/activity/DemoActivity.java
+++ app/src/main/java/com/example/askquestionpoll/activity/DemoActivity.java
@@ -1,15 +1,10 @@
 package com.example.askquestionpoll.activity;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
 
 import com.example.askquestionpoll.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DemoActivity extends AppCompatActivity {
 
